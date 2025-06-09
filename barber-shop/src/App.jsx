116ccbf5f04485a0d0ef40a609c3148dd4dac2b5
Darkpenguin1 <Frank.E.Peraza@outlook.com>
2025-06-08 22:58:37 -0400

    capital letter change

--- barber-shop/src/App.jsx
+++ barber-shop/src/App.jsx
@@ -1,7 +1,7 @@
 import './index.css';
 import { Button } from '@mui/material';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/home';
+import Home from './pages/Home';
 import Contact from './pages/Contact';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
